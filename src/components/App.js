d51aaa74b8de620e91a5f036e2e66662d4bebb70
jiaqi-jackie <jiaqili2001023@gmail.com>
2020-10-29 23:10:59 -0500

    setlist

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Header from "./Header";
+import Footer from "./Footer";
+import Main from "./Main";
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+// <header className="App-header">
+//   <img src={logo} className="App-logo" alt="logo" />
+//   <p>
+//     Edit <code>src/App.js</code> and save to reload.
+//   </p>
+//   <a
+//       className="App-link"
+//       href="https://reactjs.org"
+//       target="_blank"
+//       rel="noopener noreferrer"
+//   >
+//     Learn React
+//   </a>
+// </header>
